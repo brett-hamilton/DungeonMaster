2e0be5afef6f4fc7509659f5b869e4ebe368dddd
Norwoodg <NORWOODG@ETSU.EDU>
2021-06-11 23:06:44 -0400

    removing the old webapp

--- DungeonMaster/obj/Release/net5.0/RazorDeclaration/Pages/DiceTest.razor.g.cs
+++ DungeonMaster/obj/Release/net5.0/RazorDeclaration/Pages/DiceTest.razor.g.cs
@@ -12,91 +12,98 @@ namespace DungeonMaster.Pages
     using System.Linq;
     using System.Threading.Tasks;
 #nullable restore
-#line 1 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 1 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 2 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 3 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 4 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 5 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 6 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 7 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 8 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 9 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using DungeonMaster;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 10 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using DungeonMaster.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\_Imports.razor"
+#line 11 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
 using DungeonMaster.Pages.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\Pages\DiceTest.razor"
+#line 12 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\_Imports.razor"
+using DungeonMaster.Pages.Models;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\Pages\DiceTest.razor"
 using DungeonMaster.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\Pages\DiceTest.razor"
+#line 4 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\Pages\DiceTest.razor"
 using Microsoft.AspNetCore.Components;
 
 #line default
@@ -111,7 +118,7 @@ using Microsoft.AspNetCore.Components;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 54 "C:\Users\pokel\source\repos\DungeonMaster\DungeonMaster\Pages\DiceTest.razor"
+#line 86 "C:\Users\pokel\Source\Repos\DungeonMaster\DungeonMaster\Pages\DiceTest.razor"
        
     /// <summary>
     /// Total from dice roll, displayed to user.
@@ -130,11 +137,17 @@ using Microsoft.AspNetCore.Components;
     private string modifier = "";
 
     /// <summary>
+    /// List of previous actions taken.
+    /// </summary>
+    public List<string> GameLog { get; set; } = new List<string>();
+
+    /// <summary>
     /// Method to roll a single D20 die without modifiers.
     /// </summary>
     private void Roll()
     {
         diceTotal = Die.RollD20();
+        GameLog.Add($"Player rolled 1 D20 for {diceTotal}");
     }
 
     /// <summary>
@@ -145,7 +158,9 @@ using Microsoft.AspNetCore.Components;
     {
         try
         {
-            diceTotal = Die.Roll(dieSides, dieToRoll);
+            var rollReport = Die.Roll(dieSides, dieToRoll);
+            diceTotal = rollReport.GetDiceTotal();
+            GameLog.Add($"Player rolled {rollReport.GetDiceReport()}");
         }
         catch (Exception e)
         {
@@ -163,17 +178,25 @@ using Microsoft.AspNetCore.Components;
         if (modifier == "a")
         {
             diceTotal = Die.RollD20Advantage();
+            GameLog.Add($"Player rolled 1 D20 for {diceTotal}");
         }
 
         else if (modifier == "d")
         {
             diceTotal = Die.RollD20Disadvantage();
+            GameLog.Add($"Player rolled 1 D20 for {diceTotal}");
         }
 
         else
         {
             diceTotal = Die.RollD20();
+            GameLog.Add($"Player rolled 1 D20 for {diceTotal}");
         }
+    }
+
+    private void ClearLog()
+    {
+        GameLog = new List<string>();
     }
 
 #line default
