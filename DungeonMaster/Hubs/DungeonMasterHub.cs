f2e57020abf49d9db3a4cd2f717c95ed715309ac
Jordan DeBord <ZJAD35@ETSU.EDU>
2021-06-13 11:42:02 -0400

    Added SignalR for modal.

---
+++ DungeonMaster/Hubs/DungeonMasterHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DungeonMaster.Hubs
+{
+    public class DungeonMasterHub : Hub
+    {
+        public async Task SendMessage(string message, string page)
+        {
+            await Clients.All.SendAsync("MessageReceived", message, page);
+        }
+    }
+}
