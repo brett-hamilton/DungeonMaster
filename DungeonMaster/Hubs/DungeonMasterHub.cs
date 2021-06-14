46681b1d18ba3f1721eb029c281eba4018174d70
JDeBordTN <ZJAD35@ETSU.EDU>
2021-06-13 20:32:30 -0400

    Updated comments.

--- DungeonMaster/Hubs/DungeonMasterHub.cs
+++ DungeonMaster/Hubs/DungeonMasterHub.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace DungeonMaster.Hubs
 {
+    /// <summary>
+    /// Hub used to send and receive messages from Clients.
+    /// Based upon the Microsoft SignalR Tutorial.
+    /// </summary>
     public class DungeonMasterHub : Hub
     {
+        /// <summary>
+        /// Asynchronously Send messages to all connected clients.
+        /// Based upon the Microsoft SignalR tutorial.
+        /// </summary>
+        /// <param name="message">Message to be sent to all clients.</param>
+        /// <param name="page">Page which the message applies to.</param>
+        /// <returns></returns>
         public async Task SendMessage(string message, string page)
         {
             await Clients.All.SendAsync("MessageReceived", message, page);
